9eae3922a98560ab66d4bdf2815bee6581883ff5
NetanelV <emailofnetanel@gmail.com>
2021-10-05 23:13:00 +0300

    Remove unnecesary code

--- frontend/src/pages/feed/Feed.js
+++ frontend/src/pages/feed/Feed.js
@@ -1,22 +1,19 @@
-import { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import http from '../../axios';
-import { Like, CreatePost } from '../../components';
-import Cookies from 'universal-cookie';
-import { Button, Typography, Paper, Divider } from '@mui/material';
-
-// import * as yup from 'yup';
-// import { yupResolver } from '@hookform/resolvers/yup';
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import http from "../../axios";
+import { Like, CreatePost } from "../../components";
+import Cookies from "universal-cookie";
+import { Button, Typography, Paper, Divider } from "@mui/material";
 
 export const Feed = () => {
   const [posts, setPosts] = useState([]);
   const history = useHistory();
   const cookies = new Cookies();
-  const userId = cookies.get('userId');
+  const userId = cookies.get("userId");
 
   const getAllPosts = async () => {
     try {
-      const response = await http.get('/posts');
+      const response = await http.get("/posts");
       setPosts(response?.data);
     } catch (error) {
       console.log(error);
@@ -32,12 +29,12 @@ export const Feed = () => {
       <CreatePost posts={posts} setPosts={setPosts} />
 
       {posts.length > 0 && (
-        <Typography component='h2' variant='h2'>
+        <Typography component="h2" variant="h2">
           Posts from all the users:
         </Typography>
       )}
 
-      {posts.map(post => {
+      {posts.map((post) => {
         const likesLength = post?.likes?.length;
         const userAlreadyLikedPost = post?.likes?.includes(userId);
 
@@ -46,10 +43,10 @@ export const Feed = () => {
             <Button onClick={() => history.push(`/${post.userId}`)}>
               {post.author}
             </Button>
-            <p style={{ fontWeight: 'bold', fontSize: '2.2rem' }}>
+            <p style={{ fontWeight: "bold", fontSize: "2.2rem" }}>
               {post.title}
             </p>
-            <p style={{ marginBottom: '2rem' }}>{post.description}</p>
+            <p style={{ marginBottom: "2rem" }}>{post.description}</p>
 
             <Divider />
 
